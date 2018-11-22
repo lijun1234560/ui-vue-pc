9e461ef91af764e65ccae2e670678e41f87b6e98
qiaodaima <674059309@qq.com>
2018-11-22 21:58:41 +0800

    move before components

--- doc/doc.vue
+++ doc/doc.vue
@@ -1,14 +1,51 @@
 <template>
   <div class="doc-content">
     <h1>基础组件文档说明</h1>
+
+    <DocProgressCircle></DocProgressCircle>
+    <DocTransfer></DocTransfer>
+    <DocDialog></DocDialog>
+    <DocSideBar></DocSideBar>
+    <DocAlert></DocAlert>
+    <DocLoading></DocLoading>
+    <DocInput></DocInput>
+    <DocTextarea></DocTextarea>
+    <DocRadio></DocRadio>
+    <DocCheckbox></DocCheckbox>
+    <DocSwitch></DocSwitch>
+    <DocSelect></DocSelect>
   </div>
 </template>
 
 <script>
+  import DocProgressCircle from './doc-progress-circle.vue';
+  import DocTransfer from './doc-transfer.vue';
+  import DocDialog from './doc-dialog.vue';
+  import DocSideBar from './doc-side-bar.vue';
+  import DocAlert from './doc-alert.vue';
+  import DocLoading from './doc-loading.vue';
+  import DocInput from './doc-input.vue';
+  import DocTextarea from './doc-textarea.vue';
+  import DocRadio from './doc-radio.vue';
+  import DocCheckbox from './doc-checkbox.vue';
+  import DocSwitch from './doc-switch.vue';
+  import DocSelect from './doc-select';
+
   export default {
     name: 'Doc',
     components: {
-
+      DocProgressCircle,
+      DocTransfer,
+      DocDialog,
+      DocSideBar,
+      DocAlert,
+      DocLoading,
+      DocInput,
+      DocTextarea,
+      DocRadio,
+      DocCheckbox,
+      DocSwitch,
+      DocSelect
     }
   };
 </script>
